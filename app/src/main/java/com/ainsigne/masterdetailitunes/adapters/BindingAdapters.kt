42c86333f6f60bb3cfac0041a67f1e7c44361bd7
Andre Insigne <cominteract@gmail.com>
2020-10-04 13:24:10 +0800

    Update ui to be semi heterogenous

--- app/src/main/java/com/ainsigne/masterdetailitunes/adapters/BindingAdapters.kt
+++ app/src/main/java/com/ainsigne/masterdetailitunes/adapters/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.ainsigne.masterdetailitunes.adapters
 
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,6 +8,7 @@ import androidx.databinding.BindingAdapter
 import com.ainsigne.masterdetailitunes.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import java.util.*
 
 /**
  * Data Binding adapters specific to the app.
@@ -43,6 +45,68 @@ fun bindTrackPrice(view: TextView, text: String?) {
 fun bindTrackGenre(view: TextView, text: String?) {
     if (!text.isNullOrEmpty()) {
         view.text = String.format("%s %s", view.context.getString(R.string.track_genre), text)
+    }
+}
+
+
+/**
+ * Binds random color to a view
+ */
+@BindingAdapter("bindRandomColor")
+fun bindRandomColor(view: View, imageUrl: String?) {
+    val r = Random()
+    val num: Int = r.nextInt(10 - 1) + 1
+    when (num) {
+        0 -> {
+            view.setBackgroundColor(Color.RED)
+        }
+        1 -> {
+            view.setBackgroundColor(Color.BLUE)
+        }
+        2 -> {
+            view.setBackgroundColor(Color.GREEN)
+        }
+        3 -> {
+            view.setBackgroundColor(Color.DKGRAY)
+        }
+        4 -> {
+            view.setBackgroundColor(Color.BLACK)
+        }
+        5 -> {
+            view.setBackgroundColor(Color.YELLOW)
+        }
+        6 -> {
+            view.setBackgroundColor(Color.MAGENTA)
+        }
+        7 -> {
+            view.setBackgroundColor(Color.CYAN)
+        }
+        8 -> {
+            view.setBackgroundColor(Color.LTGRAY)
+        }
+        9 -> {
+            view.setBackgroundColor(Color.RED)
+        }
+        else -> {
+            view.setBackgroundColor(Color.BLACK)
+        }
+    }
+
+}
+
+
+/**
+ * Binds the image from the url to the imageview as a circle
+ */
+@BindingAdapter("normalImageFromUrl")
+fun bindNormalImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .fitCenter()
+            .placeholder(R.drawable.ic_launcher_background)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
     }
 }
 
